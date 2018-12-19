e4e3e5cbbeac66e553148fb451a52c289d5d8953
poomtykung <prince_se@windowslive.com>
2018-12-19 15:51:49 +0700

    Create simple webapp

--- src/main/java/com/example/demo/PreSpringProjectApplication.java
+++ src/main/java/com/example/demo/PreSpringProjectApplication.java
@@ -1,11 +1,24 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@Controller
 public class PreSpringProjectApplication {
 
+	@ResponseBody
+	@RequestMapping("/hello")
+	String ety() {
+		return "Hello world";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PreSpringProjectApplication.class, args);
 	}
